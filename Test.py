de35efa87d7cf8b8b9f3877d3b1813349a24537c
Balogh Gábor <bgs6871@yahoo.com>
2015-01-14 22:19:07 +0100

    add

--- Test.py
+++ Test.py
@@ -1,3 +1,63 @@
+print "Start"
 
-print "Hello world"
-print 3+2
+def decoratorFunctionWithArguments(arg1, arg2, arg3):
+    def wrap(f):
+        print "Inside wrap()"
+        def wrapped_f(*args):
+            print "Inside wrapped_f()"
+            print "Decorator arguments:", arg1, arg2, arg3
+            f(*args)
+            print "After f(*args)"
+        return wrapped_f
+    return wrap
+
+def decoratorFunctionWithOutArguments(f):
+    def wrap1(f):
+        print "Inside wrap1()"
+        def wrapped_f(*args):
+            print "Inside wrapped_f()"
+            print "Decorator arguments:", arg1, arg2, arg3
+            f(*args)
+            print "After f(*args)"
+        return wrapped_f
+    return wrap1
+
+
+
+
+@decoratorFunctionWithOutArguments
+def sayHello1(a1, a2, a3, a4):
+    print 'sayHello arguments:', a1, a2, a3, a4
+
+
+
+@decoratorFunctionWithArguments("hello", "world", 42)
+def sayHello(a1, a2, a3, a4):
+    print 'sayHello arguments:', a1, a2, a3, a4
+
+print "After decoration"
+
+print sayHello1("say","hello1","1","2")
+
+print "Preparing to call sayHello()"
+sayHello("say", "hello", "argument", "list")
+print "after first sayHello() call"
+sayHello("a", "different", "set of", "arguments")
+print "after second sayHello() call"
+"""
+Here's the output:
+
+Inside wrap()
+After decoration
+Preparing to call sayHello()
+Inside wrapped_f()
+Decorator arguments: hello world 42
+sayHello arguments: say hello argument list
+After f(*args)
+after first sayHello() call
+Inside wrapped_f()
+Decorator arguments: hello world 42
+sayHello arguments: a different set of arguments
+After f(*args)
+after second sayHello() call
+"""
